f1db5e6a55e9603afc24a3ab72a23ff434b2ca04
Sandwhiched <sandwh1ched.real@gmail.com>
2025-02-13 07:11:23 -0600

    Add Package class to packages.hh and create .gitignore

--- psi/packages.hh
+++ psi/packages.hh
@@ -10,4 +10,31 @@ Implementations in `packages.cc`.
 #ifndef PACKAGES_HH
 #define PACKAGES_HH
 
+#include <string>
+#include <vector>
+
+// A package.
+class Package {
+    public:
+        // The name of the package.
+        std::string name;
+
+        // The version number of the package in semantic versioning syntax.
+        std::string version;
+
+        // The architecture the binary/ies are for.
+        std::string architecture;
+
+        // The dependencies for this package.
+        std::vector<std::string> dependencies;
+
+        // Create a new Package.
+        Package(const std::string& name,
+            const std::string& version,
+            const std::string& architecture,
+            std::vector<std::string> dependencies) :
+            name(name), version(version),
+            architecture(architecture), dependencies(dependencies) {}
+};
+
 #endif // PACKAGES_HH
