c8ba72c26c9fc733d07a1e3fb7b34c94bccfe7a5
Sandwhiched <sandwh1ched.real@gmail.com>
2025-02-13 18:33:30 -0600

    General work

--- README.md
+++ README.md
@@ -3,11 +3,16 @@
 
 ## Hierarchy
 ### Documentation
-`README.md` - What you're reading right now. Good job!
-`COPYING` - The GNU GPL v3.
+`README.md` - What you're reading right now. Good job!  
+`COPYING` - The GNU GPL v3.  
 
 ### Sources
 `psi` - `libpsi` headers.
+
+### Headers
+`packages.hh` - Anything to do with the management of packages.  
+`dependencies.hh` - Dependency resolution.  
+`files.hh` - Extraction and creation of packages.  
 
 ## Credits/Inspiration
 [RPM](https://rpm.org) - Name inspiration.
--- psi/packages.hh
+++ psi/packages.hh
@@ -13,9 +13,13 @@ Implementations in `packages.cc`.
 #include <string>
 #include <vector>
 
+
 // A package.
 class Package {
     public:
+        // The path to the package file, if this package is from a package file.
+        std::string path; // TODO: Add to constructor
+
         // The name of the package.
         std::string name;
 
@@ -28,13 +32,25 @@ class Package {
         // The dependencies for this package.
         std::vector<std::string> dependencies;
 
-        // Create a new Package.
-        Package(const std::string& name,
-            const std::string& version,
-            const std::string& architecture,
-            std::vector<std::string> dependencies) :
+        // The files this package will install. Directories will be created.
+        std::vector<std::string> files;
+
+        // The configuration files/directories the package will use. Directories will be created.
+        std::vector<std::string> configurationFiles;
+
+        // Create a new `Package` object.
+        Package(const std::string& name, const std::string& version,
+            const std::string& architecture, std::vector<std::string> dependencies,
+            std::vector<std::string> files, std::vector<std::string> configurationFiles) :
             name(name), version(version),
-            architecture(architecture), dependencies(dependencies) {}
+            architecture(architecture), dependencies(dependencies),
+            files(files), configurationFiles(configurationFiles) {}
+
+        // Install this package to the system.
+        bool install();
+
+        // Remove this package from the system.
+        bool remove();
 };
 
 #endif // PACKAGES_HH
